0365b2dc0ee6c730f0f9d3f1a670ddfac9d0fb8f
rohit <choudharyrohit313131@gmail.com>
2022-10-15 15:26:29 +0530

    Timer function implemented exe file updated with latest code

--- digital_clock.c
+++ digital_clock.c
@@ -13,9 +13,9 @@ void instructions_for_clock();
 void clock();
 
 int main(){
-    printf("        Author : Rohit Choudhary\n");
-    printf("        github : https://github.com/r9hit/ \n\n");
-    clock();
+    printf("\t\t\t Author : Rohit Choudhary\n");
+    printf("\t\t\t github : https://github.com/r9hit/ \n\n");
+    choice();
     return 0;
 }
 
@@ -23,8 +23,8 @@ void choice(){
     int option; 
     read:
     printf("Choose one option :\n");
-    printf("\n Press 1 for setting timer\n"); 
-    printf("\n Press 2 for setting timer\n");
+    printf("\n Press 1 for setting the timer\n"); 
+    printf("\n Press 2 for setting the clock\n");
     scanf("%d",&option);
     if(option ==1){
         timer();
@@ -35,6 +35,48 @@ void choice(){
     else{
      goto read;
     }
+}
+
+void timer(){
+    int min,sec;
+    int delay = 1000;
+    printf("\n Rules for setting the timer  :-\n");
+    printf("\n Timer will take minutes and seconds as input as follows :\n");
+    printf("Minutes limit : 0 to 60\n");
+    printf("Second limit   : 0 to 59\n");
+
+    printf("\nSet Timer : \n");
+    printf("Enter minutes : ");
+    scanf("%d",&min);
+    printf("Enter seconds : ");
+    scanf("%d",&sec);
+
+       if(min>60 || sec>59){
+            printf("Error! \n Set the timer correctly \n ");
+            exit(0);
+        }
+       
+       while(sec || min ){
+            printf("\n \t\t\t\t Timer : " );
+            printf("\n \t\t\t\t ________________");
+            printf("\n \t\t\t\t|  %02d    |  %02d   | ",min ,sec);
+            printf("\n \t\t\t\t|________|_______|");
+            Sleep(delay); 
+            system("cls");
+
+            if(min>0 && sec == 0){
+                min--;
+                sec =60;
+            }
+            sec--;
+
+            if(sec == 3 && min == 0 ){
+                printf("Hurry Up!  \n 3 Sec remaining");
+            }
+            if(sec ==0 && min == 0 ){
+                printf("Time up!");
+            }
+        }
 }
 
 /* function for display instructions for clock*/
